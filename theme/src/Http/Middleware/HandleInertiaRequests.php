abc0e625d802c77b265664f4bf8710391b22837d
Amit Biswas <amit.biswas@cgi.com>
2022-08-13 16:54:46 +0530

    2 - Create console command to acquire all dependency

--- theme/src/Http/Middleware/HandleInertiaRequests.php
+++ theme/src/Http/Middleware/HandleInertiaRequests.php
@@ -3,10 +3,11 @@
  * Copyright © 2022. All rights reserved.
  * See COPYING.txt for license details.
  *
- * @author    CGI <info.de@cgi.com>
+ * @author    AB & PP <amit.biswas.webdev@gmail.com & priyankapal0995@gmail.com>
  * @copyright 2022
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
  
 namespace Easy\Theme\Http\Middleware;
 
